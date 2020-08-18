caec61818244c6359bd1ef7a33aa212029b33de3
MrDexII <free.andrew.a@gmail.com>
2020-08-18 20:01:50 +0200

    User DAO

--- src/main/java/com/andrzej/RESTfullPokemonAPI/repositorie/UserRepository.java
+++ src/main/java/com/andrzej/RESTfullPokemonAPI/repositorie/UserRepository.java
@@ -1,10 +1,24 @@
 package com.andrzej.RESTfullPokemonAPI.repositorie;
 
-import com.andrzej.RESTfullPokemonAPI.auth.ApplicationUser;
-import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.andrzej.RESTfullPokemonAPI.auth.ApplicationUser;
+
+import java.util.List;
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<ApplicationUser, Long> {
+public interface UserRepository {
+
+    ApplicationUser save(ApplicationUser var1);
+
+    Optional<ApplicationUser> findById(Long var1);
+
+    boolean existsById(Long var1);
+
+    List<ApplicationUser> findAll();
+
+    void deleteById(Long var1);
+
     Optional<ApplicationUser> findByUsername(String username);
+
+    void delete(com.andrzej.RESTfullPokemonAPI.auth.ApplicationUser user);
 }
