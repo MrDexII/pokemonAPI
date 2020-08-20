987cbf7f64dded287d137d6f93827379c0078084
MrDexII <free.andrew.a@gmail.com>
2020-08-20 16:01:20 +0200

    UserController change

--- src/main/java/com/andrzej/RESTfullPokemonAPI/repositorie/UserRepository.java
+++ src/main/java/com/andrzej/RESTfullPokemonAPI/repositorie/UserRepository.java
@@ -2,5 +2,8 @@ package com.andrzej.RESTfullPokemonAPI.repositorie;
 
 import com.andrzej.RESTfullPokemonAPI.auth.ApplicationUser;
 
+import java.util.Optional;
+
 public interface UserRepository extends DAORepository<ApplicationUser, Long> {
+    Optional<ApplicationUser> findByUsername(String username);
 }
