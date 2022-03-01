ef14202c0e5cbcd6779ce8c72982969ea66c4ccf
MrDexII <free.andrew.a@gmail.com>
2022-03-01 18:53:20 +0100

    mongo replica set config

---
+++ src/main/java/com/andrzej/RESTfullPokemonAPI/elasticsearch/controller/ElasticsearchController.java
@@ -0,0 +1,2 @@
+package com.andrzej.RESTfullPokemonAPI.elasticsearch.controller;public class ElasticserchController {
+}
---
+++ src/main/java/com/andrzej/RESTfullPokemonAPI/elasticsearch/repository/MyElasticsearchRepository.java
@@ -0,0 +1,9 @@
+package com.andrzej.RESTfullPokemonAPI.elasticsearch.repository;
+
+import com.andrzej.RESTfullPokemonAPI.model.Pokemon;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface sadasdqwesz extends ElasticsearchRepository<Pokemon, String> {
+}
