7dbe79e124d6d011f4e4a7ecccb7fbe3b7f0e0e3
MrDexII <free.andrew.a@gmail.com>
2022-04-21 14:43:18 +0200

    remove unnesesery method

--- src/main/java/com/andrzej/RESTfullPokemonAPI/repositorie/PokemonRepository.java
+++ src/main/java/com/andrzej/RESTfullPokemonAPI/repositorie/PokemonRepository.java
@@ -1,8 +1,6 @@
 package com.andrzej.RESTfullPokemonAPI.repositorie;
 
 import com.andrzej.RESTfullPokemonAPI.model.Pokemon;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -13,6 +11,4 @@ public interface PokemonRepository extends MongoRepository<Pokemon, String> {
     Optional<Pokemon> findByName(String name);
 
     Optional<Pokemon> findByNumber(Integer number);
-
-    Page<Pokemon> findAll(Pageable page);
 }
